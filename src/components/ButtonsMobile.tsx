656de5090ddc042919fed8169b50e6cd69a8181e
JW-dev0505 <shcherbakarina6@gmail.com>
2025-01-06 13:10:46 -0800

    store colors back for now

--- src/components/ButtonsMobile.tsx
+++ src/components/ButtonsMobile.tsx
@@ -25,7 +25,7 @@ const ButtonsMobile = () => {
       </button> */}
 
       <button
-        className="bg-[#FFD600] hover:opacity-80 text-white px-3 py-2 rounded-md transition-colors duration-200 cursor-pointer"
+        className="bg-[#2a2b3c] hover:bg-[#3a3b4c] text-white px-3 py-2 rounded-md transition-colors duration-200 cursor-pointer"
         onClick={handleHintSelected}
       >
         Hint
@@ -41,13 +41,29 @@ const ButtonsMobile = () => {
       <button
         className={`px-6 py-2 rounded-md transition-colors duration-200 cursor-pointer ${
           !gameStarted
-            ? "bg-[#43A047]"
-            : "bg-[#1E88E5]"
-        } text-white hover:opacity-80`}
+            ? "bg-green-600 hover:bg-green-400"
+            : "bg-[#2a2b3c] hover:bg-[#3a3b4c]"
+        } text-white`}
         onClick={ ()=> gameStarted ? setShowConfirmModal(true) : handlePlay() }
       >
         {gameStarted ? "Restart" : "Play"}
       </button>
+    {/* <button
+          className="bg-[#FFD600] hover:opacity-80 text-white px-3 py-2 rounded-md transition-colors duration-200 cursor-pointer"
+          onClick={handleHintSelected}
+        >
+          Hint
+        </button>
+        <button
+          className={`px-6 py-2 rounded-md transition-colors duration-200 cursor-pointer ${
+            !gameStarted
+              ? "bg-[#43A047]"
+              : "bg-[#1E88E5]"
+          } text-white hover:opacity-80`}
+          onClick={ ()=> gameStarted ? setShowConfirmModal(true) : handlePlay() }
+        >
+          {gameStarted ? "Restart" : "Play"}
+        </button> */}
     </div>
   
   )
