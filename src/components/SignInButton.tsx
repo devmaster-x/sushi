f36acc072c7d7416a3d32896ac4f1fe5db17673c
JW-dev0505 <shcherbakarina6@gmail.com>
2025-01-29 16:55:34 +0900

    update google login

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -3,33 +3,6 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Component() {
   const { data: session } = useSession()
 
-  const popupCenter = (url: string, title: string) => {
-    const dualScreenLeft = window.screenLeft ?? window.screenX;
-    const dualScreenTop = window.screenTop ?? window.screenY;
-
-    const width =
-      window.innerWidth ?? document.documentElement.clientWidth ?? screen.width;
-
-    const height =
-      window.innerHeight ??
-      document.documentElement.clientHeight ??
-      screen.height;
-
-    const systemZoom = width / window.screen.availWidth;
-
-    const left = (width - 500) / 2 / systemZoom + dualScreenLeft;
-    const top = (height - 550) / 2 / systemZoom + dualScreenTop;
-
-    const newWindow = window.open(
-      url,
-      title,
-      `width=${500 / systemZoom},height=${550 / systemZoom
-      },top=${top},left=${left}`
-    );
-
-    newWindow?.focus();
-  };
-
   if (session) {
     return <button
       className="bg-[#704337] hover:bg-[#3a3b4c] text-white px-4 py-3 rounded-md transition-colors duration-200 cursor-pointer"
@@ -40,9 +13,7 @@ export default function Component() {
   }
   return <button
     className="bg-[#704337] hover:bg-[#3a3b4c] text-white px-4 py-3 rounded-md transition-colors duration-200 cursor-pointer"
-    onClick={() => {
-      popupCenter("/google-signin", "Google Sign In");
-    }}
+    onClick={() => signIn()}
   >
     Sign in
   </button>
