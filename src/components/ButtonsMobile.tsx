24c17f43e57b90e262b1b29e8ec49550d0171249
JW-dev0505 <shcherbakarina6@gmail.com>
2025-02-18 05:38:14 +0800

    align center

--- src/components/ButtonsMobile.tsx
+++ src/components/ButtonsMobile.tsx
@@ -25,30 +25,30 @@ const ButtonsMobile = () => {
         src="assets/modal/buttons/Hint.png"
         alt="Hint"
         onClick={handleHintSelected}
-        className="cursor-pointer hover:opacity-80 w-24"
+        className="cursor-pointer hover:opacity-80 w-24 justify-self-center"
       />
 
       <img
         src={gameStarted ? 'assets/modal/buttons/playagain.png' : 'assets/modal/buttons/play.png' }
         alt="Restart"
         onClick={handlePlay}
-        className="cursor-pointer hover:opacity-80 w-24"
+        className="cursor-pointer hover:opacity-80 w-24 justify-self-center"
       />
+
+      { currentUser!.lastRound && <img
+        src="assets/images/load.png"
+        alt="Restart"
+        onClick={() => currentUser!.lastRound && handleLoad()}
+        className="cursor-pointer hover:opacity-80 w-24 justify-self-center"
+      />
+      }
 
       {gameStarted &&<img
         src="assets/images/save.png"
         alt="Save"
         onClick={() => gameStarted && handleSave()}
-        className="cursor-pointer hover:opacity-80 w-24"
+        className="cursor-pointer hover:opacity-80 w-24 justify-self-center"
       />}
-
-      { !currentUser && currentUser!.lastRound && <img
-        src="assets/images/load.png"
-        alt="Restart"
-        onClick={() => currentUser!.lastRound && handleLoad()}
-        className="cursor-pointer hover:opacity-80 w-24"
-      />
-      }
 
       {/* <button
         className="bg-[#2a2b3c] hover:bg-[#3a3b4c] text-white px-4 py-2 rounded-md transition-colors duration-200 cursor-pointer"
