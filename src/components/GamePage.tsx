0b359b3409acbdbbf06e74161c4bed9d4ee2f6b1
JW-dev0505 <shcherbakarina6@gmail.com>
2025-02-18 17:27:20 +0800

    loading fixed

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -177,7 +177,7 @@ const GameBoard = () => {
       { showSettingsModal && <Settings /> }
       { showGuide && <HowToPlay /> }
       {loading && (
-        <div className="absolute inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50 h-full">
           <div className="w-16 h-16 border-4 border-gray-500 border-dashed rounded-full animate-spin slow-spin"></div>
         </div>
       )}
